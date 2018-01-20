3088ebdc0e69c403a00a91bcc36c5ec00d44a018
LozioAlce <paolo.lombardi90@gmail.com>
2018-01-20 19:05:24 +0100

    Update PLOTTING_SCRIPT3.m
    Changes to make plotting more efficient

--- PLOTTING_SCRIPT3.m
+++ PLOTTING_SCRIPT3.m
@@ -1,26 +1,27 @@
-figure(15),hold on
-pos1 = [0.04 0.75 .5 0.22]; % [left bottom width height]
+kk=15;
+set(groot, 'CurrentFigure', kk)
+pos1 = [0.04 0.75 .95 0.22]; % [left bottom width height]
 subplot('Position',pos1)
 hold on
 plot(time,de),grid on,hold on
 set(gca,'XTickLabel','')
 ylabel(ETICHETTA(end-3))
 
-pos2 = [0.04 0.52 .5 0.2]; % [left bottom width height]
+pos2 = [0.04 0.52 .95 0.2]; % [left bottom width height]
 subplot('Position',pos2)
 hold on
 plot(time,da),grid on,hold on
 set(gca,'XTickLabel','')
 ylabel(ETICHETTA(end-2))
 
-pos3 = [0.04 0.29 .5 0.2]; % [left bottom width height]
+pos3 = [0.04 0.29 .95 0.2]; % [left bottom width height]
 subplot('Position',pos3)
 hold on
 plot(time,dr),grid on,hold on
 set(gca,'XTickLabel','')
 ylabel(ETICHETTA(end-1))
 
-pos4 = [0.04 0.06 .5 0.2]; % [left bottom width height]
+pos4 = [0.04 0.06 .95 0.2]; % [left bottom width height]
 subplot('Position',pos4)
 xlabel('Time')
 plot(time,dt),grid on,hold on
