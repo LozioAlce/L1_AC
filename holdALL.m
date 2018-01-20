d6b304113877c9f84859df00cb4dae57ce0e3b02
LozioAlce <paolo.lombardi90@gmail.com>
2018-01-20 19:30:05 +0100

    Update holdALL.m

--- holdALL.m
+++ holdALL.m
@@ -1,9 +1,6 @@
 function [T] = holdALL(INPUT)
-%DOCKNEWFIG Summary of this function goes here
-%   Detailed explanation goes here
+
 FIG = get(0,'Children');
-
-
 
 for kk = max(size(FIG)):-1:1
     figure(FIG(kk)) 
