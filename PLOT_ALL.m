ceadfe43a9151c585348b7c1f4b4c197fa19c3ff
LozioAlce <paolo.lombardi90@gmail.com>
2018-01-21 15:34:27 +0100

    Add files via upload

---
+++ PLOT_ALL.m
@@ -0,0 +1,31 @@
+close all
+set(0, 'DefaultFigureVisible', 'off');
+set(groot, 'DefaultFigureVisible', 'off');
+ cc=waitbar(1/(MAX_RUN+1))
+ kkk=1
+for ll=1:MAX_RUN+1
+	
+
+
+    BODY_Variables = BODY_Variables_RUNs(ll);
+    ACTUATOR_TRUE = ACTUATOR_RUNs(ll);
+  
+    PLOTTING_SCRIPT
+    if max(time)>199
+        
+        PLOTTING_SCRIPT2
+        PLOTTING_SCRIPT3
+        PLOTIING_SCRIPT4        
+        
+       
+    end
+    
+   waitbar(ll/(MAX_RUN+1),cc);
+end
+set(groot, 'CurrentFigure', 14)
+
+plot(time,Reference_Altitude_NOMINAL,'k','Linewidth',4)
+
+
+AddLegend(MAX_RUN+1);HideFig
+close(cc)
