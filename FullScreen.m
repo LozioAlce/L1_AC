dd6994c67c119c9b66b289791271722d63943801
LozioAlce <paolo.lombardi90@gmail.com>
2018-01-21 10:45:27 +0100

    Update FullScreen.m

--- FullScreen.m
+++ FullScreen.m
@@ -1,9 +1,10 @@
 function [] = FullScreen()
+
 FIG = get(0,'Children');
 
 for kk = max(size(FIG)):-1:1
-    figure(FIG(kk))
-    set(FIG(kk),'units','normalized','outerposition',[0 0 1 1])
+    figure(FIG(kk));
+    set(FIG(kk),'units','normalized','outerposition',[0 0 1 1]);
 end
 
 end
