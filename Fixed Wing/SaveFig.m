4a66f26878b1ed35a57914a61c9c120c92615c68
Lozio <paolo.lombardi90@gmail.com>
2018-04-28 13:06:09 +0200

    Delete SaveFig.m
    Removing all file and replacing them with new version

--- Fixed Wing/SaveFig.m
+++
@@ -1,9 +0,0 @@
-function SaveFig()
-FIG = get( 0 , 'Children' );
-
-for kk = max ( size (FIG) ): -1 : 1
-   h = figure(FIG(kk))
-    legend(gca,'off')
-    print -depsc2 -painters  -r1600 %-loose
-end
-end
